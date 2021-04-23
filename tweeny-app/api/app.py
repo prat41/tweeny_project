b76384c9ebc0e7f09b0132e2f6db48620e012577
Prathaemsh <gogawaleprathamesh@gmail.com>
2021-04-23 12:58:16 +0530

    Integrate with mongoDB

--- tweeny-app/api/app.py
+++ tweeny-app/api/app.py
@@ -49,53 +49,67 @@
 
 # if __name__ == '__main__':
 #     app.run()
-user_data = [{
-        'email' : 'gogawaleprathamesh@gmail.com',
-        'password': 'prathamesh42'
-        },
-        {
-            'email' : 'prathamesh@gmail.com',
-            'password': 'prathamesh42'
-        },
-        {
-            'email' : 'nilesh@gmail.com',
-            'password': 'prathamesh42'
-        },
-        {
-            'email' : 'siddhesh@gmail.com',
-            'password': 'prathamesh42'
-        },
-        {
-            'email' : 'tweeny@gmail.com',
-            'password': 'prathamesh42'
-        },
 
-]
+# encode = jwt.encode({"email":"gogawaleprathamesh@gmail.com","password":"prathamesh42"}, key, algorithm= "HS256")
+# print(type(encode))
+# print(jwt.decode(encode, key, algorithms="HS256"))
+
+
+
+
+
+# user_data = [{
+#         'email' : 'gogawaleprathamesh@gmail.com',
+#         'password': 'prathamesh42'
+#         },
+#         {
+#             'email' : 'prathamesh@gmail.com',
+#             'password': 'prathamesh42'
+#         },
+#         {
+#             'email' : 'nilesh@gmail.com',
+#             'password': 'prathamesh42'
+#         },
+#         {
+#             'email' : 'siddhesh@gmail.com',
+#             'password': 'prathamesh42'
+#         },
+#         {
+#             'email' : 'tweeny@gmail.com',
+#             'password': 'prathamesh42'
+#         },
+
+# ]
 
 from flask import Flask,request,Response,jsonify
 from flask_cors import cross_origin, CORS
 import datetime
+import pymongo
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 import jwt
+
 key = 'Secret'
+URI = "mongodb://127.0.0.1:27017"
+Client = pymongo.MongoClient(URI)
+Db = Client["User"]
+user_data = Db["user_data"]
 
-
-encode = jwt.encode({"email":"gogawaleprathamesh@gmail.com","password":"prathamesh42"}, key, algorithm= "HS256")
-print(type(encode))
-print(jwt.decode(encode, key, algorithms="HS256"))
 
 @app.route('/api/users/signin', methods=["POST"])
 @cross_origin()
 def login():
     if request.method == 'POST':
         data = request.get_json()
-        print(data)
-        for i in user_data:
-            if i['email'] == data['email'] and i['password'] == data['password']:
-                encode = jwt.encode(i, key, algorithm="HS256")
-                print(encode)
-                return {"msg":"Success",'access_token':encode}
+        # print(data)
+        # print(user_data.find_one({'email': data['email'], 'password':data['password']}))
+        if user_data.find_one({'email': data['email'], 'password':data['password']}) != None:
+            encode = jwt.encode(data, key, algorithm="HS256")
+           
+            return {"msg":"Success",'access_token':encode}
+
+        
+               
 
     return {"msg":"Invalid Credential"} 
         
@@ -103,25 +117,37 @@ def login():
     # print(user_data)
     # return "got it"
 
+def parse(request):
+    access_token = request.headers.get('acess_token')
+    return access_token
+    ''' 
+    1. Parse authoriazation header
+    2. decode token 
+    3. return payload.
+    '''
+
+
+
 @app.route("/api/users/my-profile", methods=["GET"])
 @cross_origin()
 def my_profile():
     if request.method =="GET":
-        access_token = request.args.get('access_token')
-        print(type(access_token))
-
+        access_token = request.headers.get('Authorization')
+        # print((access_token))
+        # print(access_token.strip("Bearer"))
+        # decode = jwt.decode(access_token, key, algorithms="HS256")
         try:
             decode = jwt.decode(access_token, key, algorithms="HS256")
+            # print("I am decode")
             email = decode['email']
-            for i in user_data:
-                if i["email"] == email:
-                    return {'msg':"Welcome {}".format(email)}
+            if user_data.find_one(decode):
+                return {'msg':"Welcome {}".format(email)}
         except Exception as e:
             return {"msg":str(e)}
 
-        # print(decode)
        
-        
+       
+    # return {'msg': 'welcome {}'.format(access_token)}   
     
 
         # return {"access_token":access_token}
@@ -164,13 +190,21 @@ Response Body:
 '''
 
 @app.route('/api/users/signup', methods = ["POST"])
+@cross_origin()
 def signup():
     if request.method == 'POST':
         data = request.get_json()
-        user_data.append(data)
+       
+        if user_data.find_one({'email': data['email']}) == None:
+            user_data.insert_one(data)
+            return {'msg':'Success'}
+        
+
+    return {'msg': "User Already Exist Please login"}
+
         
    
-    return "Signup Successfully"
+    
 
 
 if __name__ == '__main__':
